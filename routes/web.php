77e3acfdd1ad9fc3b2ff64e25d0da81cce192666
Ptitlu42 <lucas.beyer@gmx.fr>
2025-05-23 22:01:44 +0200

    Ajouter une route de test pour Orders API

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/api/test", function () {
+    return response()->json([
+        "status" => "success",
+        "service" => "orders-api",
+        "message" => "Le service Orders API fonctionne correctement",
+        "data" => [
+            "orders_count" => 18,
+            "pending_orders" => 3,
+            "version" => "1.0.0"
+        ]
+    ]);
 });
